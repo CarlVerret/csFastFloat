98dd18b97ad1e6e79c05d6f08ababc1c711adb5a
Carl Verret <carl.verret@vgq.qc.ca>
2021-01-30 18:01:42 -0500

    New benchmark

---
+++ Benchmark2/Program.cs
@@ -0,0 +1,97 @@
+﻿using Benchmark2;
+using System;
+using System.Diagnostics;
+using csFastFloat;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace Benchmark2
+{
+  internal class Program
+  {
+    public delegate double ParsingFunc(string[] values);
+
+    static internal Tuple<double, double> time_it_ns<T>(string[] lines, ParsingFunc sut, long repeat)
+    {
+      double average = 0;
+      double min_value = double.MaxValue;
+
+      Stopwatch sw = new Stopwatch();
+
+      for (int i = 0; i != repeat; i++)
+      {
+        sw.Restart();
+        sut(lines);
+
+        sw.Stop();
+        var dif = sw.ElapsedMilliseconds * 1000000;
+
+        average += dif;
+
+        min_value = min_value < dif ? min_value : dif;
+      }
+
+      average /= repeat;
+      return new Tuple<double, double>(min_value, average);
+    }
+
+    internal static string[] GetLinesFromFile(string fileName) =>
+        System.IO.File.ReadAllLines(fileName);
+
+    private static double find_max_fast_float(string[] lines)
+    {
+      double x;
+      double answer = 0;
+      for (int i = 0; i != lines.Length; i++)
+      {
+        x = FastParser.ParseDouble(lines[i]);
+        answer = answer > x ? answer : x;
+      }
+      return answer;
+    }
+
+    private static double find_max_double_parse(string[] lines)
+    {
+      double x;
+      double answer = 0;
+      for (int i = 0; i != lines.Length; i++)
+      {
+        x = Double.Parse(lines[i], CultureInfo.InvariantCulture);
+        answer = answer > x ? answer : x;
+      }
+
+      return answer;
+    }
+
+    static private void pretty_print(double volume, uint number_of_floats, string name, Tuple<double, double> result)
+    {
+      double volumeMB = volume / (1024.0 * 1024.0);
+      Console.Write("{0,-40}: {1,8:f2} MB/s (+/- {2:f1} %) ", name, volumeMB * 1000000000 / result.Item1, (result.Item2 - result.Item1) * 100.0 / result.Item2);
+      Console.Write("{0,8:f2} Mfloat/s  ", number_of_floats * 1000 / result.Item1);
+      Console.Write(" {0,8:f2} ns/f \n", (double)result.Item1 / number_of_floats);
+    }
+
+    private static void Main(string[] args)
+    {
+      string fileName = @"D:\TELUQ\Maitrise\fastfloat\simple_fastfloat_benchmark\data\canada.txt";
+      var lines = GetLinesFromFile(fileName);
+      int volume = 0;
+      foreach (string l in lines)
+      {
+        volume += l.Length;
+      }
+
+      double volumeMB = volume / (1024.0 * 1024.0);
+      Console.WriteLine($"Volume : {volumeMB}");
+
+      process_test(lines, (double)volume);
+    }
+
+    private static void process_test(string[] lines, double volume)
+    {
+      pretty_print(volume, (uint)lines.Length, "Double.Parse", time_it_ns<double>(lines, find_max_double_parse, 100));
+      pretty_print(volume, (uint)lines.Length, "FastParser.ParseDouble", time_it_ns<double>(lines, find_max_fast_float, 100));
+    }
+  }
+}
