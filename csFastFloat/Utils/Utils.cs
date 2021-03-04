1fdb21318656636af5dbf4cc252e7e8ab7fe9197
Carl Verret <carl.verret@vgq.qc.ca>
2021-03-04 16:44:33 -0500

    formating

--- csFastFloat/Utils/Utils.cs
+++ csFastFloat/Utils/Utils.cs
@@ -226,43 +226,23 @@ namespace csFastFloat
       uint hi = (uint)(value >> 32);
  
       if (hi == 0)
-      {
         return 32 + Log2SoftwareFallback((uint)value);
-      }
  
       return Log2SoftwareFallback(hi);
       
       [MethodImpl(MethodImplOptions.AggressiveInlining)]
       static int Log2SoftwareFallback(uint value)
       {
-       if (value == 0)         return 32;
-      int n = 1;
-      if (value >> 16 == 0) { n += 16; value <<= 16; }
-      if (value >> 24 == 0) { n +=  8; value <<=  8; }
-      if (value >> 28 == 0) { n +=  4; value <<=  4; }
-      if (value >> 30 == 0) { n +=  2; value <<=  2; }
-      n -= (int) (value >> 31);
-      return n;
-
+        if (value == 0)         return 32;
+        int n = 1;
+        if (value >> 16 == 0) { n += 16; value <<= 16; }
+        if (value >> 24 == 0) { n +=  8; value <<=  8; }
+        if (value >> 28 == 0) { n +=  4; value <<=  4; }
+        if (value >> 30 == 0) { n +=  2; value <<=  2; }
+        n -= (int) (value >> 31);
+        return n;
     
-            //// No AggressiveInlining due to large method size
-            //// Has conventional contract 0->0 (Log(0) is undefined)
- 
-            //// Fill trailing zeros with ones, eg 00010010 becomes 00011111
-            //value |= value >> 01;
-            //value |= value >> 02;
-            //value |= value >> 04;
-            //value |= value >> 08;
-            //value |= value >> 16;
- 
-            //// uint.MaxValue >> 27 is always in range [0 - 31] so we use Unsafe.AddByteOffset to avoid bounds check
-            //return Unsafe.AddByteOffset(
-            //    // Using deBruijn sequence, k=2, n=5 (2^5=32) : 0b_0000_0111_1100_0100_1010_1100_1101_1101u
-            //    ref MemoryMarshal.GetReference(Log2DeBruijn),
-            //    // uint|long -> IntPtr cast on 32-bit platforms does expensive overflow checks not needed here
-            //    (IntPtr)(int)((value * 0x07C4ACDDu) >> 27));
-        
-      }
+        }
 #endif
     }
 
