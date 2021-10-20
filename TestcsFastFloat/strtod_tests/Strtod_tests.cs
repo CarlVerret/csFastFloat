47f23a3788cd73f167094da8a87372a4fd4bb0d0
Carl Verret <carl.verret@vgq.qc.ca>
2021-10-19 21:55:48 -0400

    adding Strtod_tests

---
+++ TestcsFastFloat/strtod_tests/Strtod_tests.cs
@@ -0,0 +1,89 @@
+﻿using csFastFloat;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestcsFastFloat.Tests;
+using Xunit;
+
+namespace TestcsFastFloat.suppl_tests
+{
+
+  public class Strtod_tests : BaseTestClass
+  {
+
+    /// <summary>
+    /// strtod test cases
+    /// https://github.com/ahrvoje/numerics/blob/master/strtod/strtod_tests.toml
+    /// </summary>
+    [Fact]
+    void strtod_tests_file()
+    {
+      Skip.If(base.NoDiffToolDetected(), "No diff tool detected");
+
+      StringBuilder sb = new StringBuilder();
+
+      var fs = System.IO.File.OpenText(@"strtod_tests\\strtod.txt");
+      int counter = 0;
+      while (!fs.EndOfStream)
+      {
+        string curntLine = fs.ReadLine();
+
+
+        if (curntLine.StartsWith("[[")) {
+          sb.AppendLine("");
+          sb.AppendLine("");
+          sb.AppendLine(curntLine);
+
+
+
+        }
+
+
+
+
+        if (curntLine.StartsWith("UID") || curntLine.StartsWith("int"))
+          {
+            sb.AppendLine(curntLine);
+
+          }
+
+        if (curntLine.StartsWith("str ="))
+        {
+
+          sb.AppendLine(curntLine);
+          var sut = curntLine.Split('=')[1].Replace("\"", "").Trim();
+
+
+          if (!double.TryParse(sut, out double _db_val))
+          {
+            sb.AppendLine("d   = *** can't parse *** ");
+          }
+          else
+          {
+            sb.AppendLine($"d   = {_db_val}");
+          }
+
+          if (!FastDoubleParser.TryParseDouble(sut, out double _ff_val))
+          {
+            sb.AppendLine("ff   = *** can't parse *** ");
+          }
+          else
+          {
+            sb.AppendLine($"ff  = {_ff_val}");
+          }
+
+
+        }
+
+        counter++;
+      }
+      fs.Close();
+
+      ApprovalTests.Approvals.Verify(sb.ToString());
+    }
+
+  }
+}
